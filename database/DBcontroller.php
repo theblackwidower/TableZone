adef036da7d1916ecb37a448afc62901ba5162f1
rmeruliya <meruliy@uwindsor.ca>
2022-07-24 12:55:43 -0400

    changes

---
+++ database/DBcontroller.php
@@ -0,0 +1,35 @@
+<?php
+
+class DBcontroller
+{
+    protected $host = "localhost";
+    protected $user = "root";
+    protected $password = "";
+    protected $database = "tablezone";
+
+    public $con = null;
+
+    // call constructor
+    public function __construct()
+    {
+        $this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+        if ($this->con->connect_error){
+            echo "Fail " . $this->con->connect_error;
+        }
+
+    }
+
+    public function __destruct()
+    {
+        $this->closeConnection();
+    }
+
+    // for mysqli closing connection
+    protected function closeConnection(){
+        if ($this->con != null ){
+            $this->con->close();
+            $this->con = null;
+        }
+    }
+}
+
